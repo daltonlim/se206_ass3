7217d3a84610148d4ed3a69b42ed4a42d4d9f6a9
Dalton Lim <39323194+daltonlim@users.noreply.github.com>
2018-09-14 00:21:02 +1200

    Refactor a few lines for less reliance on each other

--- src/Backend/FileManager.java
+++ src/Backend/FileManager.java
@@ -12,15 +12,28 @@ import java.util.List;
  */
 public class FileManager {
     private static final File directory = new File("./audioFiles");
-    private HashMap<String,List<File>> fileList;
+    private HashMap<String, List<File>> fileList;
 
-    public FileManager(){
+    public FileManager() {
         fileList = new HashMap<>();
         getFiles();
+        System.out.println(getUniqueNames());
     }
 
-    private void getFiles(){
-        File [] wavFiles = directory.listFiles(new FilenameFilter() {
+    public List<String> getUniqueNames() {
+        return new ArrayList<>(fileList.keySet());
+    }
+
+    public List<String> getFileDatesForName(String name) {
+        List<String> stringList = new ArrayList<>();
+        for (File file : fileList.get(name)) {
+            stringList.add(getDate(file));
+        }
+        return stringList;
+    }
+
+    private void getFiles() {
+        File[] wavFiles = directory.listFiles(new FilenameFilter() {
             @Override
             public boolean accept(File directory, String name) {
                 return name.endsWith(".wav");
@@ -32,18 +45,18 @@ public class FileManager {
         }
     }
 
-    private void addFile(File file){
+    private void addFile(File file) {
         String key = getUserName(file);
-        if(fileList.get(key) == null){
+        if (fileList.get(key) == null) {
             List<File> toAdd = new ArrayList<>();
             toAdd.add(file);
-            fileList.put(key, toAdd );
-        }else   {
+            fileList.put(key, toAdd);
+        } else {
             fileList.get(key).add(file);
         }
     }
 
-    private String getUserName(File file){
+    private String getUserName(File file) {
         //Split filename
         String entireName = file.getName();
         String[] parts = entireName.split("_");
@@ -52,6 +65,15 @@ public class FileManager {
         String nameExt = parts[3];
 
         //Get isolated userName
-        return nameExt.substring(0,nameExt.length()-4);
+        return nameExt.substring(0, nameExt.length() - 4);
+    }
+
+    private String getDate(File file) {
+        //Split filename
+        String entireName = file.getName();
+        String[] parts = entireName.split("_");
+
+        //Return Date Part
+        return parts[1] + "_" + parts[2];
     }
 }
